367913d8e65343f41c9fd60b26472d2dd105dea6
dori.ji <dori.ji@feisu.com>
2022-09-20 10:16:37 +0800

    a

--- src/Services/AliPayService.php
+++ src/Services/AliPayService.php
@@ -1,19 +1,6 @@
 <?php
 namespace Dori\ali\Services;
 
-//header('Content-type:text/html; Charset=utf-8');
-//$pid = 'xxxxx';  //https://open.alipay.com 账户中心->密钥管理->mapi网关产品密钥->合作伙伴身份（PID）
-//$returnUrl = 'http://www.xxx.com/alipay/return.php';     //付款成功后的同步回调地址
-//$notifyUrl = 'http://www.xxx.com/alipay/notify.php';     //付款成功后的异步回调地址
-//$outTradeNo = uniqid();     //你自己的商品订单号
-//$payAmount = 0.01;          //付款金额，单位:元
-//$orderName = '支付测试';    //订单标题
-//$signType = 'RSA';       //签名算法类型，RSA或MD5，推荐使用RSA
-////商户私钥，对应签名算法填写
-//$privateKey='MIICWwIBAAKBgQCaYZmbUPalRCqo6a5x7Wbl4x2ZaYbTZ5Vmh0h2LDnwhsrHW6yLAe/DQNtc/kwWGXdcUvAZEz+KCmIdDeRDXIoK/fKMkC2/a/kCeimrp7X+EPaSFCEYI/OZ9Kr99LIOlaWGbs5UM8YQaPY8CUz935/0u4vov/zCUr/OGV3Z7vvIRQIDAQABAoGAMgzYXulFvVi3N+Hiw17DEF6sVvNGWb1oS0Aqtt0pv5gDn9RRwRNUpwmc1K5A/p0s/EqLOqXp4JfzLRY23bvfqKW19GzNfxB16wyeN46U+lYlFDfhbGOb9GMqqCdROXCMkfesbQjrBKP/KD2xkT/u5O5qIMunI5P4HZ1bqXwaUMECQQDKMgdEi+Pf0oZkptvVfI9ogNoj9bSStFazY1TcbDsY8smmwgQ73FLE4+43tr2j+cBWqSc8Ead27Zs9FBcByfgRAkEAw3Ze+KQvQ0SBCqn0w94p9FytQ3FaU1k3Pa1DjFV6+snRDnr71v7FbJn4W9AGgFl609FnNmW/GBGOWmMaE/tg9QJAP/AaBD8ITBrSjLmHArdk0fMNCBoGg+2rDQb4Ksu+1GDSC32GwTcmpUQ+bDfJODUd8UmEMqaSgjUYxfO4YsRvQQJAE1diPG5asuhTcI+yxbL1KdYF2xebXYYFflfnyDopKinQQwOOag7Z0E5IEiW6jvbLvawJ74sP5rzN/kmyKeoFBQJAMhwuIKyNob0qBGRFL4NaNiRGuEbVChlIM5dbzC8OTAhwU5jAbexfIUNxweiZB62vkpLMgeEUWOSVb+ngggxrpA==';
-//$aliPay = new AliPayService($pid,$returnUrl,$notifyUrl,$signType,$privateKey);
-//$sHtml = $aliPay->doPay($payAmount,$outTradeNo,$orderName,$returnUrl,$notifyUrl);
-//echo $sHtml;
 class AliPayService
 {
     protected $appId;
@@ -42,7 +29,7 @@ class AliPayService
      * @param string $timestamp 订单发起时间
      * @return string
      */
-    public function doPay($totalFee, $outTradeNo, $orderName)
+    public function pay($totalFee, $outTradeNo, $orderName)
     {
         $requestConfigs = array(
             'partner'=>$this->appId,
@@ -70,7 +57,7 @@ class AliPayService
         $para = $this->buildRequestPara($para_temp);
 
         $sHtml = "<form id='alipaysubmit' name='alipaysubmit' action='https://mapi.alipay.com/gateway.do?_input_charset=".trim(strtolower($this->charset))."' method='post'>";
-        while (list ($key, $val) = each ($para)) {
+        foreach ($para as $key=>$val){
             $sHtml.= "<input type='hidden' name='".$key."' value='".$val."'/>";
         }
         //submit按钮控件请不要含有name属性
@@ -184,14 +171,15 @@ class AliPayService
      */
     private function createLinkstring($para) {
         $arg  = "";
-        while (list ($key, $val) = each ($para)) {
+        foreach ($para as $key=>$val){
             $arg.=$key."=".$val."&";
         }
         //去掉最后一个&字符
-        $arg = substr($arg,0,count($arg)-2);
+        $arg = substr($arg,0,count((array)$arg)-2);
 
         //如果存在转义字符，那么去掉转义
-        if(get_magic_quotes_gpc()){$arg = stripslashes($arg);}
+        define('MAGIC_QUOTES_GPC',ini_set("magic_quotes_runtime",0)?True:False);
+        if(MAGIC_QUOTES_GPC){$arg = stripslashes($arg);}
 
         return $arg;
     }
@@ -203,7 +191,7 @@ class AliPayService
      */
     private function paraFilter($para) {
         $para_filter = array();
-        while (list ($key, $val) = each ($para)) {
+        foreach ($para as $key=>$val){
             if($key == "sign" || $key == "sign_type" || $val == "")continue;
             else	$para_filter[$key] = $para[$key];
         }
